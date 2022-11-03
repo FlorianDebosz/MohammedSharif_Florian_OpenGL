1df21e50087606199148155301ccdd4478830e14
Shrams <mohammedsharifa@gmail.com>
2022-11-03 10:28:51 +0100

    window
    possibility to draw a polygon and a window with different colors

--- MathsOpenGL/main.cpp
+++ MathsOpenGL/main.cpp
@@ -8,14 +8,20 @@ using namespace std;
 int width = 640;
 int height = 480;
 
+array<float, 3> polyColor = {1,1,1};
+array<float, 3> windColor = {1,1,1};
+
 static int mainMenu;
 static int subMenuColor;
 
 array<int, 2> currentPoint;
 vector<array<int, 2>> points;
+vector<array<int, 2>> windowPoints;
 
 bool closed = false;
+bool windowClosed = false;
 bool drawPoly = false;
+bool drawWind = false;
 
 //draw the line between the points selected by user
 void drawPolygon()
@@ -24,9 +30,29 @@ void drawPolygon()
 	{
 		glBegin(GL_LINE_STRIP);
 
+		glColor3f(polyColor[0], polyColor[1], polyColor[2]);
+
 		for (auto& pt : points)
 			glVertex2f((float)pt[0], (float)pt[1]);
 		auto& endPt = closed ? points.front() : currentPoint;
+		glVertex2f((float)endPt[0], (float)endPt[1]);
+
+		glEnd();
+	}
+}
+
+//draw the window with the user inputs
+void drawWindow()
+{
+	if (!windowPoints.empty())
+	{
+		glBegin(GL_LINE_STRIP);
+
+		glColor3f(windColor[0], windColor[1], windColor[2]);
+
+		for (auto& pt : windowPoints)
+			glVertex2f((float)pt[0], (float)pt[1]);
+		auto& endPt = windowClosed ? windowPoints.front() : currentPoint;
 		glVertex2f((float)endPt[0], (float)endPt[1]);
 
 		glEnd();
@@ -41,6 +67,9 @@ void render()
 	//call drawPolygon
 	drawPolygon();
 
+	//call drawWindow
+	drawWindow();
+
 	glutSwapBuffers();
 }
 
@@ -52,21 +81,32 @@ void keyboard(unsigned char c, int x, int y)
 		closed = true;
 		drawPoly = false;
 	}
-	//press esc key to clear the polygon finished or the polygon in construction
-	if (c == 27)
-		points.clear();
+	//press f to close the window and stop window drawing
+	if (c == 'f')
+	{
+		windowClosed = true;
+		drawWind = false;
+	}
 }
 
 //manage mouse events
 void mouse(int button, int state, int x, int y)
 {
-	//if left mouse button is down then add the next point
+	//if left mouse button is down then add the next point for polygon
 	if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN && drawPoly)
 	{
 		if (closed)
 			points.clear();
 		closed = false;
 		points.push_back(currentPoint);
+	}
+	//if left mouse button is down then add the next point for window
+	if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN && drawWind)
+	{
+		if (windowClosed)
+			windowPoints.clear();
+		windowClosed = false;
+		windowPoints.push_back(currentPoint);
 	}
 }
 
@@ -85,19 +125,30 @@ void menu(int n)
 	//choose color once the user started drawing the polygon
 	case 11 :
 		if (drawPoly)
-			glColor3f(1.0,0,0);
+			polyColor = { 1.0,0,0 };
+		if (drawWind)
+			windColor = { 1.0,0,0 };
 		break;
 	case 12:
 		if (drawPoly)
-			glColor3f(0, 1.0, 0);
+			polyColor = { 0,1.0,0 };
+		if (drawWind)
+			windColor = { 0,1.0,0 };
 		break;
 	case 13:
 		if (drawPoly)
-			glColor3f(0, 0, 1.0);
+			polyColor = { 0,0,1.0 };
+		if (drawWind)
+			windColor = { 0,0,1.0 };
 		break;
 	//start the drawing process of a polygon when user click on the appropriate menu
 	case 2 :
 		drawPoly = true;
+		points.clear();
+		break;
+	case 3:
+		drawWind = true;
+		windowPoints.clear();
 		break;
 	default :
 		break;
